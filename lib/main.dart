b650bfb281cf3f01f06dfbc81633a19caac3d021
shehabahmed <ShehabAhmed22>
2023-09-03 13:46:17 +0300

    car_ui with a simple animation(clean code)

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:carUI/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import 'View/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(builder: (context, orientation, screenType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Rent a Car',
+        theme: lightThemeData(context),
+        darkTheme: darkThemeData(context),
+        home: const Home(),
+      );
+    });
+  }
+}
